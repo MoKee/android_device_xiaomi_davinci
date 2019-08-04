db48f133afb1dae18dcd33cbae9035c2653cb3b5
PIPIPIG233666 <2212848813@qq.com>
2019-08-03 20:05:55 -0400

    davinci: MoKee-ify
    Change-Id: I747539800bceb6dda8f2ccd7b87799eae12ba669

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_davinci.mk
+    $(LOCAL_DIR)/mk_davinci.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_davinci-userdebug \
-    lineage_davinci-eng
+    mk_davinci-userdebug \
+    mk_davinci-eng
--- device.mk
+++ device.mk
@@ -13,7 +13,7 @@ TARGET_PRODUCT_HARDWARE := davinci
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-mokee
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
 
